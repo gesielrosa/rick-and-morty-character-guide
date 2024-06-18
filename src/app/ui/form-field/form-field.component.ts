8b7fad3cd560afe3cc7269380142edd6fe76f25b
gesielrosa <git@gesiel.com>
2024-06-18 01:08:06 -0300

    feat: create form field component

---
+++ src/app/ui/form-field/form-field.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-form-field',
+  standalone: true,
+  templateUrl: './form-field.component.html',
+  styleUrl: './form-field.component.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class FormFieldComponent {}
