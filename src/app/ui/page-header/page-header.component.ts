ecbe5e2269f8cb0cf11faf8578e577f55dd37d10
gesielrosa <git@gesiel.com>
2024-06-18 01:07:58 -0300

    feat: create page header component

---
+++ src/app/ui/page-header/page-header.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-page-header',
+  standalone: true,
+  templateUrl: './page-header.component.html',
+  styleUrl: './page-header.component.scss',
+  imports: [],
+})
+export class PageHeaderComponent {
+  public title = input.required<string>();
+}
